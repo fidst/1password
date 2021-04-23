3842e138c8f4e72d484438c4c1ed284bd30ac119
Isaac Fidst <fidst@users.noreply.github.com>
2021-04-23 17:41:19 -0400

    99% faster

--- remove_sus.ps1
+++ remove_sus.ps1
@@ -8,7 +8,7 @@
 #                    3. To run a PS script on mac: pwsh -File [filename]
 #                    4. Uses: https://1password.com/downloads/command-line/
 #                    5. You MUST log into 1password via command PRIOR to running this script
-# Revision:          April 12, 2021
+# Revision:          April 23, 2021
 # ===================================================================
 #
 
@@ -22,14 +22,8 @@ Function sleepy(){Start-Sleep -s 2}
 $scriptTitle="1p_sus_del"
 # Get the date
 $dateStamp = get-date -uformat "%Y-%m-%d@%H-%M-%S"
-# get list of all users (converts the json to an object for easy manipulation)
-$1pusers = op list users | Convertfrom-json
-# Count of users:
-$1pcount = $1pusers.count
-# Sets count to 1 for the loop later on
-$n=1
 # 0= not test
-$runTest = 0
+$runTest = 1
 
 
 # function called later to check if user is in 1pass
@@ -53,18 +47,30 @@ function checkUser ($t) {
 # import ignore txt file. This is a seperate function to allow a file check later on
 Function getTXT()
 {
+# check if files exist
+$fileToCheckTEST    = ".\test.txt"
+$fileToCheckNOTTEST = ".\ignore.txt"
   # Checks if this is a test
   If ($runTest -eq 1)
   {
-    # This is a test, so let the user know and use test.txt
-    write-host "THIS IS A TEST - Using test.txt."
-    $Global:1pfile = ".\test.txt"
+    if (Test-Path $fileToCheckTEST -PathType leaf)
+    {
+    $Global:1pfile = ".\test.txt"}else{
+      write-host "Creating ignore files. Restart script."
+      New-Item -Name $fileToCheckTEST  -ItemType File
+      exit
+    }
       } else {
-    # This is NOT a test, so let the user know and use ignore.txt
-    write-host "THIS IS NOT A TEST - Using ignore.txt"
-    $Global:1pfile = ".\ignore.txt"
+        if (Test-Path $fileToCheckNOTTEST -PathType leaf)
+        {
+    $Global:1pfile = ".\ignore.txt"}else{
+      write-host "Creating ignore files. Restart script."
+  New-Item -Name $fileToCheckNOTTEST -ItemType File
+  exit
+    }
   }
 }
+
 
 function processText(){
 # get the ignore file
@@ -74,10 +80,13 @@ function processText(){
   # Sets the process for ignoring to true
     $Global:ignoreUsers="TRUE"
   # Displays the ignored users and pauses for dramatic effect
-  If (@($ignoreArray).Count -eq 0)
+  If (@($ignoreArray).Count -ne 0)
   {
+    next_line
+    write-host "Ignoring these users:"
     $ignoreArray
     sleepy
+    next_line
   }
 }
 
@@ -86,80 +95,76 @@ function createLOG(){
 # log all names to document
 # create blank logging csv
     # Name of the log csv (based on the title and date variables)
-    $1pImportOutput = "$scriptTitle-$dateStamp.csv"
+    $1pImportOutput = "$scriptTitle-$dateStamp-temp.csv"
     # Location of the log csv
     $Global:1pImportFile = "$1pImportOutput"
     # Tells the user that the log csv is being made
     write-host "Creating new log CSV: $1pImportFile"
     # The headers in the log csv (MUST match the array made later on)
     Set-Content $1pImportFile -Value "Email,State,NeedRemoval,Ignored,ConfirmRemoval"
-    sleepy
+
+    # Sets and creates the final log after confirming the user is or is not in 1pass
+    # Name of the log csv (based on the title and date variables)
+    $1pImportFileFinal = "$scriptTitle-$dateStamp-final.csv"
+    # Location of the log csv
+    $Global:1pImportOutputFinal = "$1pImportFileFinal"
+    Set-Content $1pImportOutputFinal -Value "Email,State,NeedRemoval,Ignored,ConfirmRemoval"
+
     next_line
 }
 
 function processUsers(){
-# Loops through each property in the user object (each user reported)
+
+
+# Loops through each property in the user object (each user reported) and sorts it into 2 arrays
+# get list of all users (converts the json to an object for easy manipulation)
+$1pusers = op list users | Convertfrom-json
+# Count of users:
+$1pcount = $1pusers.count
+# Sets count to 1 for the loop later on
+$n=1
+# Declares empty suspended user array
+$susUsers = @()
+# Declares empty not suspended user array
+$nSusUsers = @()
+# Creates a log array to be later injected into a csv (must match headers in the csv creation)
+$logFileO=New-Object PSObject -Property @{
+Email=""
+State=""
+NeedRemoval=""
+Ignored=""
+ConfirmRemoval=""
+}
+
+write-host "Sorting Users"
 foreach ($user in $1pusers)
 {
+  # Creates the initial log output made from the email and state from the object
+  $useremail = $user.email
+  $userstate = $user.state
 
-# Show the progress
-# Basic percentage math
-$nP = [math]::Round(($n/$1pcount)*100,2)
-# Shows the count and the math for each loop so you know how long you have to get coffee
-write-host "$n/$1pcount ($nP%)"
-next_line
-# Adds 1 to the loop variable to let the math continue each loop
-$n++
-
-# if for what ever reason either the email or state of a user is empty, we skip it
-  if (![string]::IsNullOrEmpty($user.state) -Or ![string]::IsNullOrEmpty($user.email))
-{
-  # Creates a log array to be later injected into a csv (must match headers in the csv creation)
-  $logFileO=New-Object PSObject -Property @{
-  Email=""
-  State=""
-  NeedRemoval=""
-  Ignored=""
-  ConfirmRemoval=""
-  }
-
-# Creates the initial log output made from the email and state from the object
-  $logFileO.Email = $useremail = $user.email
-  $logFileO.State = $userstate = $user.state
-
+# if user is suspended add to array
 # If the user has a state of "S", they are suspended
+
   If ($userstate -eq "S")
   {
-      # If the state is suspended, they need removal, lets log that
-      $logFileO.NeedRemoval      = "TRUE"
+    # if suspended, add to suspended user array
+$susUsers += $useremail
+  } else {
+    # if not suspended, add to not suspended user array
+$nSusUsers += $useremail
+  }
+} #End for loop
 
-      # DELETE USER
-# If a test, output this is a test, and the email for visual representation
-      If ($runTest -eq 1)
-      {
+write-host "Actioning Suspended Users"
+  foreach ($email in $susUsers)
+  {
+# Action the suspeded users
+$logFileO.State = "S"
+$logFileO.Email = $useremail = $email
+# If the state is suspended, they need removal, lets log that
+$logFileO.NeedRemoval = "TRUE"
 
-          write-host "THIS IS A TEST -  (Deletion process) $useremail"
-        If ($ignoreUsers="TRUE")
-        {
-          # if in array, ignore
-          If (@($ignoreArray).Count -ne 0)
-          {
-          If ($ignoreArray.Contains($useremail))
-          {
-            $logFileO.Ignored="TRUET"
-            write-host "THIS IS A TEST -  IGNORING $useremail"
-          }else{
-          $logFileO.Ignored="FALSE"
-          write-host "THIS IS A TEST -  NOT IGNORING $useremail"
-        }
-      }else{
-        $logFileO.Ignored="FALSE"
-        write-host "THIS IS A TEST -  NOT IGNORING $useremail"
-      }
-      }
-
-      }else{
-# If not a test, do the deed of deletion
 
 If ($ignoreUsers="TRUE")
 {
@@ -169,69 +174,98 @@ If ($ignoreUsers="TRUE")
   If ($ignoreArray.Contains($useremail))
   {
   $logFileO.Ignored="TRUE"
-  write-host "Ignoring the user."
+  write-host "Ignoring the user: $useremail"
   }else{
     $logFileO.Ignored="FALSE"
-    op delete user $useremail
+    If ($runTest -eq 1){write-host "THIS IS A TEST -  (Deletion process) $useremail"}
+    else {op delete user $useremail}
   }
 }else{
   $logFileO.Ignored="FALSE"
-  op delete user $useremail
+  If ($runTest -eq 1){write-host "THIS IS A TEST -  (Deletion process) $useremail"}
+  else {op delete user $useremail}
+}
 }
 
-}
+# Outputs the log array to the csv
+  $logFileO | Export-CSV $1pImportFile -Append -NoTypeInformation -Force
 
-}
+  }# end forloop
 
-# If not suspended, no action needed, log they are not suspended and move along
-  }else{
-    $logFileO.NeedRemoval = "FALSE"
-    $logFileO.Ignored     = "TRUE"
-  }
-
-  # Confirm user deletion
-# If a test, output this is a test, and the email for visual representation
-  If ($runTest -eq 1)
+  foreach ($email in $nSusUsers)
   {
-write-host "THIS IS A TEST -  USER NOT DELETED"
-# It is a test, so obviously the user is not deleted
-  $logFileO.ConfirmRemoval="NOT REMOVED"
+# Action the suspeded users
+$logFileO.State = "A"
+$logFileO.Email = $useremail = $email
+$logFileO.NeedRemoval = "FALSE"
+$logFileO.Ignored     = "TRUE"
+
+# Outputs the log array to the csv
+  $logFileO | Export-CSV $1pImportFile -Append -NoTypeInformation -Force
+
+  }# end forloop
+
+} # End function
+
+function confirmRemoval () {
+
+  # import csv
+$1pImportFileFinal = Import-Csv .\$1pImportFile
+
+  # create new csv
+# $Global:1pImportOutputFinal = "$scriptTitle-$dateStamp-final.csv"
+# Set-Content $1pImportOutputFinal -Value "Email,State,NeedRemoval,Ignored,ConfirmRemoval"
+
+  # create new object from 1password
+$1pusers = op list users | Convertfrom-json
+# Adding each email to an array
+$userEmails = @()
+Foreach ($user in $1pusers)
+{
+$userEmails += $user.email
+}
+
+write-host "Confirming removal of users"
+write-host "Logging to $1pImportOutputFinal"
+
+  # for each user in old csv, check if in 1pass object
+  foreach ($user in $1pImportFileFinal)
+  {
+    if (![string]::IsNullOrEmpty($user.Email) -Or ![string]::IsNullOrEmpty($user.State))
+  {
+    # Creates a log array to be later injected into a csv (must match headers in the csv creation)
+    $logFile1=New-Object PSObject -Property @{
+    Email=""
+    State=""
+    NeedRemoval=""
+    Ignored=""
+    ConfirmRemoval=""
+    }
+
+$logFile1.Email       = $uEmail          = $user.email
+$logFile1.State       = $uState          = $user.state
+$logFile1.NeedRemoval = $uNeedRemoval    = $user.NeedRemoval
+$logFile1.Ignored     = $uIgnored        = $user.Ignored
+
+  If ($userEmails.Contains($uEmail))
+  {
+    $logFile1.ConfirmRemoval = "FALSE"
   }else{
-# If not a test, we will check a freshly made user list for the email and report back using the function above
-# We check EVERY user as a precaution
-  $1passCheck = checkUser $useremail # Check if user exists in 1pass
-  if ($1passCheck -eq "FALSE") {
-# If the user IS NOT found, it is returned as false, and logged as REMOVED
-  $logFileO.ConfirmRemoval="REMOVED"
-  }else{
-# If the user IS found, they are not removed. This will let us review logs
-  $logFileO.ConfirmRemoval="NOT REMOVED"
+    $logFile1.ConfirmRemoval = "TRUE"
   }
+
+  # output to new csv
+  # Outputs all information for the user (based on the log array)
+  # Outputs the log array to the csv
+  $logFile1 | Export-CSV $1pImportOutputFinal -Append -NoTypeInformation -Force
 }
-
-# Outputs all information for the user (based on the log array)
-  $logFileO
-# Outputs the log array to the csv
-  $logFileO | Export-CSV $1pImportFile -Append -NoTypeInformation -Force
-
-# Shows some speration for better visuals
-next_line
-write-host "----------"
-next_line
-}
-
 
 }
 
-# Outputs all information for the user (based on the log array)
-  $logFileO
-# Outputs the log array to the csv
-  $logFileO | Export-CSV $1pImportFile -Append -NoTypeInformation -Force
-
-# end forloop
+  # delete old csv
+  write-host "Removing original import CSV"
+  Remove-Item -Path .\$1pImportFile -Force
 }
-
-
 
 
 
@@ -243,17 +277,21 @@ if (Test-Path $1pfile -PathType leaf)
 {
   If ($runTest -eq 1)
   {
+write-host "THIS IS A TEST - Using test.txt."
 processText
 createLOG
 processUsers
+confirmRemoval
 }else{
   Write-Warning "This is not a test, do you wish to run?"
   $askRun = Read-Host -Prompt '(y/n) '
   if($askRun -eq "y")
   {
+    write-host "THIS IS NOT A TEST - Using ignore.txt"
     processText
     createLOG
     processUsers
+    confirmRemoval
   }else{
     Write-host "Ending."
   }
